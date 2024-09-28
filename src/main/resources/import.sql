b06ae55e0270af6981def53aa23e20fab04821fe
Bruno Camargo <brunovieiracamargo@gmail.com>
2024-09-28 15:44:26 -0300

    feat: add postgres dev services configuration and initial database data.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,49 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+DROP TABLE IF EXISTS presets;
+
+CREATE TABLE presets(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    texture VARCHAR(255) NOT NULL,
+    models VARCHAR(255) NOT NULL,
+    anisotropic_filter VARCHAR(255) NOT NULL,
+    lighting VARCHAR(255) NOT NULL,
+    shadows VARCHAR(255) NOT NULL,
+    reflections VARCHAR(255) NOT NULL,
+    atmospherics VARCHAR(255) NOT NULL,
+    ambient_oclusion VARCHAR(255) NOT NULL,
+    tessellation VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    youtube_channel VARCHAR(255) NOT NULL,
+    video_link VARCHAR(255) NOT NULL
+);
+
+INSERT INTO presets(
+    title,
+    texture,
+    models,
+    anisotropic_filter,
+    lighting,
+    shadows,
+    reflections,
+    atmospherics,
+    ambient_oclusion,
+    tessellation,
+    name,
+    youtube_channel,
+    video_link
+)
+VALUES(
+    'God of War Ragnarok',
+    'HIGH',
+    'HIGH',
+    'ULTRA',
+    'MEDIUM',
+    'MEDIUM',
+    'MEDIUM',
+    'HIGH',
+    'ULTRA',
+    'MEDIUM',
+    'BenchmarKing',
+    'https://www.youtube.com/@benchmarking4386',
+    'https://www.youtube.com/watch?v=e6UPDPdFf4A'
+);
